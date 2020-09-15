b9c9e273635b6864a4afc4d49a54d08faaa8538d
nadir <s.nodir9494@gmail.com>
2020-09-15 13:59:09 +0300

    create project

---
+++ app/src/main/java/com/github/nodirsulaymonov/simpleapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.github.nodirsulaymonov.simpleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
